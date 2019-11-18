f1ce29a13e9ad7ae17cb701902cba01e9abda547
Dosoon Lee <dosoon.lee@hp.com>
2019-11-18 00:33:36 -0800

    add code

---
+++ ch_C_5_ControlFlow_originally_ch5/c.5.10_std::cin_extraction.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file c.5.10_std::cin_extraction.cpp
+ * @author dosoon.lee@gmail.com
+ * @brief 
+ * @version 0.1
+ * @date 2019-11-18
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ * @note
+ * 1. A program that handles error cases well is said to be robust.
+ * 2. When we use operator>> to get user input and put it into a variable, this is called an “extraction”. The >> operator is accordingly called the extraction operator when used in this context.
+ */
+
+#include <iostream>
+
+int main()
+{
+    return 0;
+}
